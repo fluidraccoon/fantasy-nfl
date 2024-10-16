dffa1f1c2287f9abf1131fe80db74d6d946a0234
fluidraccoon <danieljcoulton@gmail.com>
2024-10-16 18:47:31 +0100

    week 6 run

--- Hello.py
+++ Hello.py
@@ -36,7 +36,7 @@ max_gameweek = max(df_matchup_schedule["gameweek"])
 # with st.sidebar:
 #     gameweek_start, gameweek_end = st.slider("Select gameweeks", 1, 2, (1, 2))
 gameweek_start = 1
-gameweek_end = 5
+gameweek_end = 6
 df_matchup_schedule = df_matchup_schedule[
     (df_matchup_schedule["gameweek"] >= gameweek_start) & (df_matchup_schedule["gameweek"] <= gameweek_end)
 ]
