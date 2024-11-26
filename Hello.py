65d2fc3e024859ab4aaeb1a2815b210a8ac60ea1
fluidraccoon <danieljcoulton@gmail.com>
2024-11-26 14:44:25 +0000

    fix to playoff/bye chances for superflex keeper

--- Hello.py
+++ Hello.py
@@ -43,33 +43,39 @@ league_size = df_matchup_schedule[df_matchup_schedule["gameweek"]==1]["gameweek"
 def simulate_table():
     # Grouping by 'season' and 'Manager' and aggregating
     table_sim = (df_summary_week.groupby(['season', 'manager','division'], as_index=False)
-                .agg(Wins=('selected_win', 'sum'),
-                    Points=('selected_pts', 'sum'),
-                    PWins=('selected_pp_win', 'sum'),
-                    PPoints=('selected_pp', 'sum'),
-                    Upcoming=('nxt_wk_win', 'sum'))
+                .agg(wins=('selected_win', 'sum'),
+                    points=('selected_pts', 'sum'),
+                    potential_wins=('selected_pp_win', 'sum'),
+                    potential_points=('selected_pp', 'sum'),
+                    upcoming=('nxt_wk_win', 'sum'))
                 )
 
     # Sorting within each group by 'Points' (descending)
-    table_sim["Position"] = table_sim.sort_values(['season', 'division', 'Wins', 'Points'], ascending=[True, True, False, False])\
+    table_sim["position"] = table_sim.sort_values(['season', 'division', 'wins', 'points'], ascending=[True, True, False, False])\
         .groupby(['season', 'division']).cumcount() + 1
-    
-    # st.dataframe(table_sim)
-
-    # Adding 'Position' column based on rank within each 'season'
-    # table_sim['Position'] = table_sim.groupby('season')['Points'].rank(method='first', ascending=False).astype(int)
 
     # Adding 'Playoff' column based on condition (if Position <= 6)
-    table_sim['Playoff'] = np.where(table_sim['Position'] <= (6 if league_size==12 else 4 if league_size==10 else 1), 1, 0)
+    if league_selection == "Super Flex Keeper":
+        table_sim_wc = table_sim.copy()
+        table_sim_wc["wins"] = np.where(table_sim_wc["position"] <= 2, 0, table_sim_wc["wins"])
+        table_sim_wc["points"] = np.where(table_sim_wc["position"] <= 2, 0, table_sim_wc["points"])
+        table_sim["overall_position"] = table_sim_wc.sort_values(['season', 'wins', 'points'], ascending=[True, False, False])\
+        .groupby(['season']).cumcount() + 1
+        table_sim['playoff'] = np.where((table_sim['position'] <= 2) | (table_sim["overall_position"] <= 2), 1, 0)
+    else:
+        table_sim['playoff'] = np.where(table_sim['position'] <= (6 if league_size==12 else 4 if league_size==10 else 1), 1, 0)
 
     # Adding 'Bye' column based on condition (if Position <= 2)
-    table_sim['Bye'] = np.where(table_sim['Position'] <= 2, 1, 0)
+    if league_selection == "Super Flex Keeper":
+        table_sim['bye'] = np.where(table_sim['position'] == 1, 1, 0)
+    else:
+        table_sim['bye'] = np.where(table_sim['position'] <= 2, 1, 0)
 
     # Re-arranging again, sorting by 'Playoff' and 'PPoints' within each 'season'
-    table_sim = table_sim.sort_values(['season', 'Playoff', 'PPoints'], ascending=[True, False, False])
+    table_sim = table_sim.sort_values(['season', 'playoff', 'potential_points'], ascending=[True, False, False])
 
     # Adding 'Draft_Pos' column based on new ranking after re-arranging
-    table_sim['Draft_Pos'] = table_sim.groupby('season').cumcount() + 1
+    table_sim['draft_pos'] = table_sim.groupby('season').cumcount() + 1
 
     # Removing the groupby index (like ungroup in R)
     table_sim = table_sim.reset_index(drop=True)
@@ -94,8 +100,8 @@ def part1(df_matchup_schedule):
     df_matchup_schedule["all_play"] = df_matchup_schedule.groupby("gameweek")["points"].rank("max") - 1
     
     playoff_chances = simulate_table().groupby(["manager"]).agg(
-        playoff=("Playoff", "mean"),
-        bye=("Bye", "mean")
+        playoff=("playoff", "mean"),
+        bye=("bye", "mean")
     ).reset_index()
 
     df_standings = df_matchup_schedule.groupby(["manager"]).agg(
@@ -107,11 +113,11 @@ def part1(df_matchup_schedule):
     df_standings = df_standings.sort_values(by=["wins", "points"], ascending=False).reset_index(drop=True)
     df_standings["all_play_display"] = df_standings["all_play"].apply(lambda x: f"{x:.0f}") + "-" + (gameweek_end * (league_size - 1) - df_standings["all_play"]).apply(lambda x: f"{x:.0f}") 
     df_standings["xwins"] = round(df_standings["all_play"] / (league_size - 1), 1)
-    df_standings["accuracy"] = df_standings["points"] / df_standings["pp_points"]
+    df_standings["accuracy"] = df_standings["points"] / df_standings["pp_points"] * 100
     df_standings = df_standings.drop(columns=["pp_points", "all_play"])
     df_standings = df_standings.merge(playoff_chances, on="manager", how="left")
     df_standings.index = df_standings.index + 1
-    # df_standings = df_standings.drop(columns=["bye"])
+    
     
 
     def set_background_color(x, league_size):
@@ -128,7 +134,7 @@ def part1(df_matchup_schedule):
         "points": st.column_config.NumberColumn("Points", help="Number of points so far"),
         "all_play_display": st.column_config.TextColumn("All-Play", help="Wins and losses if you played every team each week"),
         "xwins": st.column_config.TextColumn("xWins", help="Expected number of wins based on the all-play record"),
-        "accuracy": st.column_config.ProgressColumn("Accuracy", help="Accuracy of team selection compared to maximum points", min_value=0, max_value=1),
+        "accuracy": st.column_config.ProgressColumn("Accuracy", help="Accuracy of team selection compared to maximum points", format="%.1f %%", min_value=0, max_value=100),
         "playoff": st.column_config.NumberColumn("Playoff %", help="% chance of team making the playoffs"),
         "bye": st.column_config.NumberColumn("Bye %", help="% chance of team getting a first-round bye"),
     }
