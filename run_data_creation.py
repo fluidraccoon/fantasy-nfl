58d2b9684e26a3d51a7f1c841887eda40892decf
fluidraccoon <danieljcoulton@gmail.com>
2025-11-30 22:56:26 +0000

    2025 week 12 run

--- run_data_creation.py
+++ run_data_creation.py
@@ -48,7 +48,7 @@ df_players = get_players_df()
 username = "DanCoulton"
 season = 2025
 sims = 1000
-current_gameweek = 11
+current_gameweek = 12
 
 all_leagues = get_league_dict(username, season)
 df_matchup_schedule_all_leagues = []
